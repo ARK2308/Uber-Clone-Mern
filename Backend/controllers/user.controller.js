80178edbfdc00dfd0157b6e2630797ab257f5762
ARK2308 <abdulark500@gmail.com>
2024-12-11 10:10:45 +0530

    UserLogin Done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,7 +2,7 @@ const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
 
-
+// Register Controller
 module.exports.registerUser = async (req, res, next) => {
 
     const errors = validationResult(req);
@@ -32,4 +32,34 @@ module.exports.registerUser = async (req, res, next) => {
     res.status(201).json({ token, user });
 
 
+}
+
+
+// Login Controllers
+module.exports.loginUser = async (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, user });
 }
