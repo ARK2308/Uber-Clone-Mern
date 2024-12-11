a3c7fcc7235ba5bc821a535c0a768d93057de229
ARK2308 <abdulark500@gmail.com>
2024-12-11 09:48:06 +0530

    Uber-Bakcend Setup

---
+++ Backend/app.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const userRoutes = require('./routes/user.routes');
+const db = require("./db/db");
+db.connect();
+
+
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/users', userRoutes);
+
+
+
+
+
+
+module.exports = app;
