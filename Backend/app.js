4025179f933250b16d5ab6db239037ba2c2b9c5a
ARK2308 <abdulark500@gmail.com>
2024-12-11 10:50:11 +0530

    User-Profile And User Logout Done

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv')
 dotenv.config();
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const userRoutes = require('./routes/user.routes');
 const db = require("./db/db");
@@ -14,6 +15,7 @@ db.connect();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 
